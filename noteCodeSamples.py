ee7944bcb424cf820f1e92e6a704b85379e23d65
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-01-07 15:51:39 -0800

    Update noteCodeSamples.py

--- noteCodeSamples.py
+++ noteCodeSamples.py
@@ -69,3 +69,59 @@ favorite_language.lstrip()
 #'python '
 favorite_language.strip()
 #'python'
+
+# Removing Prefixes
+# When working with strings, another common task is to remove a prefix. Consider a URL with the common prefix https://. 
+# We want to remove this prefix, so we can focus on just the part of the URL that users need to enter into an address bar
+nostarch_url = 'https://nostarch.com'
+nostarch_url.removeprefix('https://')
+#'nostarch.com'
+# Keep it permenantly 
+simple_url = nostarch_url.removeprefix('https://')
+
+# Numbers 
+# Integers: You can add (+), subtract (-), multiply (*), and divide (/) integers in Python.
+2 + 3
+#5
+3 - 2
+#1
+3 ** 2
+#9
+# Python supports the order of operations too
+2 + 3*4
+#14
+(2 + 3) * 4
+#20
+
+# Floats
+# Python calls any number with a decimal point a float. 
+0.1 + 0.1
+#0.2
+0.2 + 0.2
+#0.4
+
+# Python defaults to a float in any operation that uses a float, even if the output is a whole number.
+# HOWEVER, be aware that you can sometimes get an arbitrary number of decimal places in your answer:
+0.2 + 0.1
+#0.30000000000000004
+3 * 0.1
+#0.30000000000000004
+
+# Underscores in Numbers
+# When you’re writing long numbers, you can group digits using underscores to make large numbers more readable:
+universe_age = 14_000_000_000
+print(universe_age)
+#14000000000
+
+# Multiple Assignment
+# You can assign values to more than one variable using just a single line of code. 
+x, y, z = 0, 0, 0
+
+# Constants
+# A constant is a variable whose value stays the same throughout the life of a program. 
+# Python programmers use all capital letters to indicate a variable should be treated as a constant and never be changed
+MAX_CONNECTIONS = 5000
+
+# Comments
+# Say hello to everyone.
+print("Hello Python people!")
